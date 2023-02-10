26450252a6fd7c8a6bae6e480ce17711d5c8268e
Sagacious101 <116090716+Sagacious101@users.noreply.github.com>
2023-02-10 17:10:17 +0300

    Add files via upload

---
+++ garden.py
@@ -0,0 +1,44 @@
+import random
+
+
+garden = ["покрышка", "бутылка", "окурок", "железяка", "череп коровы"]
+garden.clear()
+order = (
+    ("ирис", "ирис", "ирис", "ирис", "ирис", "ирис", "ирис", "ирис", "ирис", "ирис"),
+    ("роза", "роза", "роза", "роза", "роза", "роза", "роза", "роза", "роза", "роза"),
+    ("пион", "пион", "пион", "пион", "пион", "пион", "пион", "пион", "пион", "пион")
+)
+for package in order:
+  for seed in package:
+    garden.append(seed)
+weeds = ("борщевик", "крапива", "лопух")
+for weed in weeds:
+  for i in range(random.randint(5, 10)):
+    garden.append(weed)
+random.shuffle(garden)
+print("сад до прополки:", garden)
+print("\nидентификатор сада до прополки:", id(garden))
+
+def weeding(garden: list, weeds: list) -> None:
+    for i in range(len(garden)):
+        for n in range(len(weeds)):
+            if garden[i] == weeds[n]:
+                garden.pop(i)
+                return weeding(garden, weeds)
+
+
+weeding(garden, weeds)
+
+print("\nсад после прополки:", garden)
+print("\nидентификатор сада после прополки:", id(garden))
+
+flowers_quantity = len(garden)
+print(f"\nобщее количество цветов: {flowers_quantity}")
+
+bouquet = []
+
+for i in range(5):
+    bouquet.append(random.choice(garden))
+
+print(f"\nбукет: {bouquet}")
+    
